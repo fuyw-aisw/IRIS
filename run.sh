281eeb8b5b96c2cbe52626aa858164805ae6badc
fuyw <118046924+fuyw-aisw@users.noreply.github.com>
2025-08-12 01:33:01 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,28 @@
-for n_percents in 0.2 0.3 0.4 0.5 0.6; do
-    echo "===== Running with n_percents = $n_percents ====="
+echo "--MUTAG--"
+nohup python main.py --dataset 'MUTAG' --num_train=50 --num_val=50 >> result_round.out & 
+wait
 
-    echo "--MUTAG--"
-    nohup python main.py --dataset 'MUTAG' --num_train=50 --num_val=50 --epochs 100 --n_percents $n_percents >> result_n_percents.out &
-    wait
+echo "--PTC-MR--"
+nohup python main.py --dataset 'PTC_MR' --num_train=90 --num_val=90 --epochs 100 >> result.out & 
+wait
 
-    echo "--PTC-MR--"
-    nohup python main.py --dataset 'PTC_MR' --num_train=90 --num_val=90 --epochs 100 --n_percents $n_percents >> result_n_percents.out &
-    wait
+echo "--DHFR--"
+nohup python main.py --dataset 'DHFR' --num_train=120 --num_val=120 --epochs 100 >> result_round.out & 
+wait
 
-    echo "--DHFR--"
-    nohup python main.py --dataset 'DHFR' --num_train=120 --num_val=120 --epochs 100 --n_percents $n_percents >> result_n_percents.out &
-    wait
+echo "--PROTEINS--"
+nohup python main.py --dataset 'PROTEINS' --num_train=300 --num_val=300 --epochs 100 >> result_round.out & 
+wait
 
-    echo "--PROTEINS--"
-    nohup python main.py --dataset 'PROTEINS' --num_train=300 --num_val=300 --epochs 100 --n_percents $n_percents >> result_n_percents.out &
-    wait
+echo "--DD--"
+nohup python main.py --dataset 'DD' --num_train=300 --num_val=300 --epochs 100 >> result_round.out & 
+wait
 
-    echo "===== Finished n_percents = $n_percents ====="
-    echo ""
-done
+
+echo "--AIDS--"
+nohup python main.py --dataset 'AIDS' --num_train=500 --num_val=500 --epochs 100 >> result_round.out & 
+wait
+
+echo "--NCI1--"
+nohup python main.py --dataset 'NCI1' --num_train=1000 --num_val=1000 --epochs 100 >> result_round.out & 
+wait
